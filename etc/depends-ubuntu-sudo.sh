7d6a207a63a065aa1dad161007b2a87ed4336f1a
Ian Dennis Miller <ian@iandennismiller.com>
2016-09-23 11:37:48 -0400

    more robust dependency management on Ubuntu systems

---
+++ etc/depends-ubuntu-sudo.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# pub2 (c) Ian Dennis Miller
+
+# This script is useful for supporting a Jekyll installation on Ubuntu 12.04 LTS.
+# This will install a specific version of Ruby, update pip and virtualenvwrapper,
+# and install TeX Live using Scott Kosty's script.
+#
+# This script must be run as root.
+
+# ensure rvm is installed
+curl -sSL https://get.rvm.io | bash -s stable --ruby
+
+# install Ruby 2.3.0
+rvm install 2.3.0
+rvm use 2.3.0
+rvm rubygems latest
+
+# ensure bundler is installed
+gem install bundler
+
+# ensure pip is current
+pip install -U pip
+
+# ensure virtualenvwrapper is installed
+pip install -U virtualenv virtualenvwrapper
+
+# install TeX Live (currently 2016)
+wget https://github.com/scottkosty/install-tl-ubuntu/raw/master/install-tl-ubuntu
+chmod +x ./install-tl-ubuntu
+./install-tl-ubuntu
--- etc/depends-ubuntu.sh
+++ etc/depends-ubuntu.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/bash
+# pub2 (c) Ian Dennis Miller
 
-# install TeX Live
-wget https://github.com/scottkosty/install-tl-ubuntu/raw/master/install-tl-ubuntu
-chmod +x ./install-tl-ubuntu
-echo "now install as root.  This will request sudo access"
-sudo ./install-tl-ubuntu
+# This script is useful for supporting a Jekyll installation on Ubuntu 12.04 LTS.
+# It will create local versions of Ruby and Python modules.  This script is intended
+# for unprivileged execution.  It has a counterpart called depends-ubuntu-sudo.sh
+# that must be run with root privileges.
+
+# for Jekyll
+source /usr/local/rvm/scripts/rvm
+bundle install --path vendor/bundle
+
+# for Pub2
+source /usr/local/bin/virtualenvwrapper.sh
+mkvirtualenv -a . -r requirements.txt pub2
+source ~/.virtualenvs/pub2/bin/activate
